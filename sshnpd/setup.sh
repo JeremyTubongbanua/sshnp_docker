0aa7305b26c613abfcd7fb4c200f1ea5ef5c0dd4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-11 22:08:58 -0400

    feat: working

--- sshnpd/setup.sh
+++ sshnpd/setup.sh
@@ -1,2 +1,1 @@
 tmux new -d -s "sshnpd" ; tmux send-keys -t sshnpd "~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v" C-m ; tmux a
-tmux new -d -s "sshnpd" ; tmux send-keys -t sshnpd "~/.local/bin/sshnpd -a @extraordinary96 -m @sagittarius19 -d docker -s -u -v" C-m ; tmux a
