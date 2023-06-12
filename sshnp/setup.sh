789e2640d87348158bbbaf52594ee9d794ec2317
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-11 21:32:46 -0400

    feat: copy and pastas

--- sshnp/setup.sh
+++ sshnp/setup.sh
@@ -1,3 +1,8 @@
-ssh-keygen -A
 ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C "john@example.com"
+$(~/.local/bin/sshnp -f @jeremy_0 -t @smoothalligator -d docker -h @rv_am -s id_ed25519.pub)
+$(~/.local/bin/sshnp -f @sagittarius19 -t @extraordinary96 -d docker -h @rv_am -s id_ed25519.pub)
+
+Or 
+
 ~/.local/bin/sshnp -f @jeremy_0 -t @smoothalligator -d docker -h @rv_am -s id_ed25519.pub -v
+~/.local/bin/sshnp -f @sagittarius19 -t @extraordinary96 -d docker -h @rv_am -s id_ed25519.pub -v
--- sshnpd/setup.sh
+++ sshnpd/setup.sh
@@ -1,2 +1,2 @@
-ssh-keygen -A
-~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -v -u -s
+tmux new -d -s "sshnpd" ; tmux send-keys -t sshnpd "~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v" C-m ; tmux a
+tmux new -d -s "sshnpd" ; tmux send-keys -t sshnpd "~/.local/bin/sshnpd -a @extraordinary96 -m @sagittarius19 -d docker -s -u -v" C-m ; tmux a
